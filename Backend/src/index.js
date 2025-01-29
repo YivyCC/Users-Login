127f7b996a9b77f959db47518269b20c34753521
Ricardo Castro <ricardo20.castro00@gmail.com>
2025-01-29 12:07:04 -0800

    updated cors for cross-origin request issues

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,7 +5,13 @@ import router from "./routes/userRoutes.js"
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+const cors = require('cors');
+app.use(cors({
+  origin: 'https://users-login-1.onrender.com/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use('/api', router);
