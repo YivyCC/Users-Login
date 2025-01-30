2c0d381b3ba6a68a3020e5aa60791d9812a40958
Ricardo Castro <ricardo20.castro00@gmail.com>
2025-01-30 10:48:22 -0800

    updated db.js to remove pool

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -1,48 +1,24 @@
-import { Pool } from 'pg';
-import env from 'dotenv';
+import pg from "pg";
+import env from "dotenv";
 
 env.config();
 
-const pool = new Pool({
+const db = new pg.Client({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
+  host:process.env.DB_HOST,
+  database:process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port:process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: false,
   },
 });
 
-pool.on('error', (err) => {
+db.connect();
+
+db.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
   process.exit(-1);
 });
 
-// Use `pool.query` for all database queries
-export const query = (text, params) => pool.query(text, params);
-
-// import pg from "pg";
-// import env from "dotenv";
-
-// env.config();
-
-// const db = new pg.Client({
-//   user: process.env.DB_USER,
-//   host:process.env.DB_HOST,
-//   database:process.env.DB_DATABASE,
-//   password: process.env.DB_PASSWORD,
-//   port:process.env.DB_PORT,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
-
-// db.connect();
-
-// db.on('error', (err) => {
-//   console.error('Unexpected error on idle client', err);
-//   process.exit(-1);
-// });
-
-// export const query = (text, params) => db.query(text, params);
+export const query = (text, params) => db.query(text, params);
